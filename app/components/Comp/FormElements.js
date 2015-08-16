77099a158d53742be046c7478ae734fe4891b7b2
Iswan Jumat <info@iswanjumat.com>
2015-08-16 16:16:57 +0530

    added textarea support

--- app/components/Comp/FormElements.js
+++ app/components/Comp/FormElements.js
@@ -1,47 +1,65 @@
 import React from 'react';
 
-class Text extends React.Component {
-	componentDidMount(){
-		console.log(this.refs.inputdef.getDOMNode().value);
-	}
+class Input extends React.Component {
+	//Set input value
 	getValue(){
-		return this.refs.input.getDOMNode().value;
+		switch(this.props.type){
+			case 'text':
+				return React.findDOMNode(this.refs.input).value;
+				break;
+			case 'textarea':
+				return React.findDOMNode(this.refs.textarea).value;
+			default:
+				return 'invalid input type';
+		}
+		
 	}
-	render() {
-		let req_star,description;
-		if(this.props.required == true){
-			req_star =  <i className="material-icons">star</i>;
-		}
+
+	//get input description
+	getDescription() {
 		if(this.props.description != ''){
-			description = <div className="description">{this.props.description}</div>;
+			return this.description = <div className="description">{this.props.description}</div>;
 		}
+	}
+
+	getRequiredIcon() {
+		if(this.props.required == true){
+			return this.req_star =  <i className="material-icons">star</i>;
+		}
+	}
+
+	renderTextInput() {
+		
 		return (
 			<div className="input-group">
-		        <label htmlFor={this.props.id}>{this.props.label} {req_star}</label>
-		        <input className={`text ${this.props.className}`} type="text" id={this.props.id} name={this.props.id} placeholder={this.props.placeholder} ref="input" defaultValue="Hello!"/>
-		        {description}
+		        <label htmlFor={this.props.id}>{this.props.label} {this.getRequiredIcon()}</label>
+		        <input {...this.props} className={`text ${this.props.className}`} type="text" ref="input"/>
+		        {this.getDescription()}
 		    </div>
 		)
+	}
+	renderTextArea() {
+		return (
+			<div className="input-group">
+		        <label htmlFor={this.props.id} >{this.props.label} {this.getRequiredIcon()}</label>
+		        <textarea {...this.props} className={`textarea ${this.props.className}`} name="comment" id="textarea" placeholder="Your comment here..." ref="textarea"></textarea>
+		        {this.getDescription()}
+		    </div>
+		)
+	}
+	render() {
+		if(this.props.type == 'text'){
+			return this.renderTextInput();
+		}else if(this.props.type == 'textarea'){
+			return this.renderTextArea();
+		}
 	}
 }
 
 Text.propTypes = {
 	label: React.PropTypes.string.isRequired,
-	id: React.PropTypes.string.isRequired,
-	placeholder: React.PropTypes.string.isRequired,
-	//ref: React.PropTypes.string.isRequired,
-	required: React.PropTypes.bool.isRequired,
-	description: React.PropTypes.string.isRequired
+	id: React.PropTypes.string.isRequired
 };
 
-class TextArea extends React.Component {
-	render() {
-		return (
-			<div>
-				TextArea
-			</div>
-		)
-	}
-}
 
-export {Text, TextArea};
+export {Input};
--- app/components/Form.js
+++ app/components/Form.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Text, TextArea} from './Comp/FormElements';
+import {Input} from './Comp/FormElements';
 
 class Form extends React.Component {
-	handleSubmit(e){
-		e.stopPropagation();
-		var username = this.refs.inputdef.getDOMNode().value;
-		console.log(username);
+	handleChange(){
+		let text = this.refs.txt.getValue();
+		console.log(text);
 	}
 	render() {
 		return (
@@ -15,8 +14,45 @@ class Form extends React.Component {
 				<div className="example">
 					<p className="head">Example</p>
 					<h4>Text Input</h4>
-					<form className="rui-form" onSubmit={this.handleSubmit.bind(this)}>
-						<Text className="additional_class" label="Default input" id="inputdef" placeholder="input placeholder" required={true} description="Your default width input." />
+					<form className="rui-form">
+						<Input type="text"
+							className="additional_class"
+							label="Default input"
+							id="inputdef"
+							placeholder="input placeholder"
+							required={true}
+							description="Your default width input." 
+						/>
+						<Input type="text"
+							className="long-input"
+							label="Long input"
+							id="inputLong"
+							description="Your long width input." 
+						/>
+						<Input type="text"
+							className="medium-input"
+							label="Medium input"
+							id="inputMedium"
+							description="Your medium width input." 
+						/>
+						<Input type="text"
+							className="short-input"
+							label="Short input"
+							id="inputShort"
+							description="Your short width input." 
+						/>
+						<Input type="text"
+							label="Disabled input"
+							id="inputDisabled"
+							placeholder="Disabled input"
+							disabled="disabled"
+						/>
+						<h3>Textarea</h3>
+						<Input type="textarea"
+							label="Comment"
+							id="inputTextarea"
+							placeholder="Disabled input"
+						/>
 					</form>
 				</div>
 			</div>
